b422d5cacd8ddce0f3638d8c4ec57bf6af5bc35a
Backwasch <Kampfkeks.tnt@gmail.com>
2024-03-02 14:09:12 +0100

    Added Timestamp and removed lock file

--- SICK-MPB10.js
+++ SICK-MPB10.js
@@ -1,5 +1,4 @@
-module.exports = function(RED)
-{   
+module.exports = function (RED) {
     function hexStringToFloat(hexString) {
         const hex = parseInt(hexString, 16);
         const sign = hex >> 31 ? -1 : 1;
@@ -7,25 +6,27 @@ module.exports = function(RED)
         return sign * (hex & 0x7fffff | 0x800000) * 1.0 / Math.pow(2, 23) * Math.pow(2, (exponent - 127));
     }
 
-    function GetValue(config)
-    {
+    function padWithZero(number) {
+        return number < 10 ? '0' + number : number;
+    }
+
+    function GetValue(config) {
         RED.nodes.createNode(this, config);
         var node = this;
 
         let ip = config.ip;
         let portStr = "/iolinkmaster/port[" + config.port + "]/iolinkdevice/pdin/getdata";
 
-        let requestObj = {"code": "request", "cid": 4, "adr": portStr};
+        let requestObj = { "code": "request", "cid": 4, "adr": portStr };
 
         let requestJSON = JSON.stringify(requestObj);
 
         const https = require('http');
 
-        if(config.refreshRate < 0.01)
+        if (config.refreshRate < 0.01)
             config.refreshRate = 0.01;
 
-        setInterval(function()
-        {
+        setInterval(function () {
             let options =
             {
                 hostname: ip,
@@ -38,13 +39,14 @@ module.exports = function(RED)
                 }
             }
 
-            let request = https.request(options, function(result)
-            {
-                result.on('data', function(data)
-                {
-                    try
-                    {
+            let request = https.request(options, function (result) {
+                result.on('data', function (data) {
+                    try {
                         var newJson = JSON.parse(data);
+
+                        let now = new Date();
+                        let formattedDate = `${padWithZero(now.getDate())}-${padWithZero(now.getMonth() + 1)}-${now.getFullYear()}`;
+                        let formattedTime = `${padWithZero(now.getHours())}:${padWithZero(now.getMinutes())}:${padWithZero(now.getSeconds())}.${now.getMilliseconds()}`;
 
                         let msg = {};
                         msg.payload =
@@ -54,28 +56,27 @@ module.exports = function(RED)
                             rmsZ: hexStringToFloat(newJson.data.value.substring(8, 16)),
                             rmsY: hexStringToFloat(newJson.data.value.substring(16, 24)),
                             rmsX: hexStringToFloat(newJson.data.value.substring(24, 32)),
-                            notImplementedYet: newJson.data.value.substring(32, 40)
+                            notImplementedYet: newJson.data.value.substring(32, 40),
+                            timestamp: `${formattedDate} ${formattedTime}` // Date and Time
 
                         }
                         node.send(msg);
                     }
-                    catch(e)
-                    {
+                    catch (e) {
                         console.log(e);
                     }
                 })
             });
 
-            request.on('error', function(error) {
+            request.on('error', function (error) {
                 //avoid ECONNRESET due to Node-Red bug
-              })
+            })
 
             request.write(requestJSON);
             request.end();
         }, config.refreshrate * 1000);
 
-        node.on('input', function(msg)
-        {
+        node.on('input', function (msg) {
 
         });
     }
